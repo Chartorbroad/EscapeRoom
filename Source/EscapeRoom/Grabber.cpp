66956032551f7e847a14b90cdbd7cb10235d9ed4
j.kempster <jacob.j.kempster@gmail.com>
2019-04-28 12:33:55 +0100

    Logging LineTrace hits to output

--- Source/EscapeRoom/Grabber.cpp
+++ Source/EscapeRoom/Grabber.cpp
@@ -4,6 +4,7 @@
 #include "Grabber.h"
 #include "Engine/World.h"
 #include "Engine/Public/DrawDebugHelpers.h"
+#include "GameFramework/Actor.h"
 #include <GameFramework/PlayerController.h>
 
 #define OUT //this does nothing at all but can be used for markup lower down - i.e. visual reminders!
@@ -36,11 +37,13 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// Get the player view point
-	PlayerController->GetPlayerViewPoint(PlayerViewPointLocation, PlayerViewPointRotator);
+	/// Get the player view point
+	PlayerController->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation, 
+		OUT PlayerViewPointRotator);
 	/*UE_LOG(LogTemp, Warning, TEXT("PlayerController View Point = %s , %s"), *PlayerViewPointLocation.ToString(), *PlayerViewPointRotator.ToString());*/
 
-	// Draw a red trace in the world to visualise
+	/// Draw a red trace in the world to visualise
 	FVector LineTraceEnd = PlayerViewPointLocation + (PlayerViewPointRotator.Vector() * Reach);//FVector(0.f, 0.f, 100.f); // temporary end vector for drawing;
 	DrawDebugLine(
 		GetWorld(),
@@ -53,9 +56,25 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 		10.f
 	);
 
-	// Ray-cast out to reach distance
+	/// Setup query parameters
+	FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
 
-	// See what we hit
+	/// Ray-cast (Line-trace) out to reach distance
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParameters
+	);
+
+	/// See if anything was hit
+	AActor* ActorHit = Hit.GetActor();
+	if (ActorHit) { // make sure it's not a null-pointer (i.e. no collison)
+		UE_LOG(LogTemp, Warning, TEXT("The LineTrace hit the Actor:: %s"), *(ActorHit->GetName()))
+	}
+		
 
 
 }
